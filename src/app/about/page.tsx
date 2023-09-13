b6e1e0e833b246650fb4078187fd3a3e90963a29
RuiMendonca1043 <120272317+RuiMendonca1043@users.noreply.github.com>
2023-09-14 00:21:49 +0100

    feat: update database schema

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import Trips from "./components/Trips";
+import React from "react";
+
+export const metadata = {
+  title: "About",
+};
+
+const Page = () => {
+  return (
+    <div>
+      <Trips />
+    </div>
+  );
+};
+
+export default Page;

4fb711e40b5f8f50777b198c10a0bee6028a81ed
RuiMendonca1043 <120272317+RuiMendonca1043@users.noreply.github.com>
2023-09-14 14:28:45 +0100

    feat: add initial search trip

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Input from "../../components/Input";
 import DatePicker from "@/components/DatePicker";
 import CurrencyInput from "@/components/CurrencyInput";
+import Button from "@/components/Button";
 
 const TripSearch = () => {
   return (
-    <div className="container mx-auto p-5">
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
       <h1 className="font-semibold text-xl text-primaryDarker p-5 text-center">
         Encontre a sua próxima <span className="text-primary">viagem!</span>
       </h1>
@@ -19,6 +20,8 @@ const TripSearch = () => {
           />
           <CurrencyInput placeholder="Orçamento" />
         </div>
+
+        <Button>Buscar</Button>
       </div>
     </div>
   );

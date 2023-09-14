455a995132dde0350daeb47d18ba98dc44321e5a
RuiMendonca1043 <120272317+RuiMendonca1043@users.noreply.github.com>
2023-09-14 14:22:34 +0100

    feat: tailwind colors; chore: header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { NextAuthProvider } from "@/providers/auth";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Header from "../components/Header";
 
 const inter = Poppins({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="pt">
       <body className={inter.className}>
         <NextAuthProvider>
+          <Header />
           {children}
         </NextAuthProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import TripSearch from "./components/TripSearch";
 
 export default function Home() {
-  const { data } = useSession();
-  data?.user;
   return (
     <div>
-      <button onClick={() => signIn()}>Login</button>
-      <h1>Olá, {data?.user?.name}</h1>
+      <TripSearch />
     </div>
   );
 }
